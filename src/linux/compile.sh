d3cc89f1a85b7b7546169b4cbadd70f3ef5682e1
HRA! <hra@mvj.biglobe.ne.jp>
2022-05-31 07:00:21 +0900

    Updated

--- src/linux/compile.sh
+++ src/linux/compile.sh
@@ -3,10 +3,11 @@
 if test -e "*.o"; then
 	rm *.o
 fi
-g++ -O2 -W -c ../zma.cpp                  -o zma.o                 
-g++ -O2 -W -c ../zma_information.cpp      -o zma_information.o     
-g++ -O2 -W -c ../zma_parse.cpp            -o zma_parse.o           
-g++ -O2 -W -c ../zma_parse_expression.cpp -o zma_parse_expression.o
-g++ -O2 -W -c ../zma_parse_process.cpp    -o zma_parse_process.o   
-g++ -O2 -W -c ../zma_text.cpp             -o zma_text.o            
+g++ -O2 -W -c -I.. -I../sub ../sub/*.cpp
+g++ -O2 -W -c -I.. -I../sub ../zma.cpp                  -o zma.o                 
+g++ -O2 -W -c -I.. -I../sub ../zma_information.cpp      -o zma_information.o     
+g++ -O2 -W -c -I.. -I../sub ../zma_parse.cpp            -o zma_parse.o           
+g++ -O2 -W -c -I.. -I../sub ../zma_parse_expression.cpp -o zma_parse_expression.o
+g++ -O2 -W -c -I.. -I../sub ../zma_parse_process.cpp    -o zma_parse_process.o   
+g++ -O2 -W -c -I.. -I../sub ../zma_text.cpp             -o zma_text.o            
 g++ -O2 *.o -o zma
