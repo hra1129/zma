91cac68bc774588bbcbef759ba1a6a837b473ca1
hsk <sakurai777@gmail.com>
2023-06-13 17:45:20 +0900

    fix return value

--- src/sub/zma_parse_process_error.hpp
+++ src/sub/zma_parse_process_error.hpp
@@ -13,7 +13,7 @@ class CZMA_PARSE_ERROR: public CZMA_PARSE {
 public:
 	using CZMA_PARSE::CZMA_PARSE;
 	bool process( CZMA_INFORMATION& info, CZMA_PARSE* p_last_line = nullptr ) override;
-	virtual bool is_parse_error( void ) {
+	virtual bool is_parse_error( void ) override {
 		return true;
 	}
 };
